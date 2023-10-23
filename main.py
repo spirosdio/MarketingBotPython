0ca58e19699bb0111c2399d0c4181a0c3442f44c
s.diochnos <s.diochnos@tradias.de>
2023-10-23 03:20:21 +0300

    add tree creation and send first message

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from test_server_client import test_server_client
+from parameters import user_node_dict, create_welcoming_message, create_coupon_message, create_media_message
+from server_communication import run_client_server, send_message_to_mock_server
+from tree import Data, TreeNode
 from user_database import UserDatabase
 import threading
-from server_communication import run_client_server
 
 if __name__ == "__main__":
     # Start the client's server in a separate thread
@@ -12,5 +13,24 @@ if __name__ == "__main__":
     db.create_user('vaso', 'kollia', 2, '27')
     db.create_user('angelos', 'todri', 3, '28')
 
-    # test_server_client()
+    # Creating data instances
+    data_root = Data(create_welcoming_message)
+    data_child1 = Data(create_coupon_message, "show_coupon")
+    data_child2 = Data(create_media_message, "no_thanks")
 
+    # Creating the tree nodes with data instances
+    root = TreeNode(data_root)
+    child1 = TreeNode(data_child1)
+    child2 = TreeNode(data_child2)
+
+    # Building the tree structure
+    root.add_child(child1)
+    root.add_child(child2)
+
+    #a dict that has a user as key and the root node as value
+    for user in db.get_all_users():
+        user_node_dict[user] = root
+        send_message_to_mock_server(root.data.json(user))
+
+    # Displaying the tree
+    root.display()
--- parameters.py
+++ parameters.py
@@ -53,3 +53,57 @@ message_read_answer = {
     "interaction_timestamp": datetime.utcnow().timestamp()
 }
 user_node_dict = {}
+
+
+def create_welcoming_message(userdb):
+    welcoming_attachment = {
+        "actions": [
+            {
+                "name": "show_coupon",
+                "text": "Yes! Show me coupon",
+                "type": "button"
+            },
+            {
+                "name": "no_thanks",
+                "text": "No, thanks",
+                "type": "button"
+            }
+        ]
+    }
+    return {
+        "user_id": userdb.user_id,
+        "callback_url": "http://localhost:6000/webhook/callback",
+        "text": f"Welcome to the demo promotional flow, {userdb.name}! Are you interested in our coupon promotion?",
+        "attachments": welcoming_attachment
+    }
+
+
+def create_coupon_message(userdb):
+    coupon_attachment = {
+        "actions": [
+            {
+                "name": "reveal_coupon",
+                "text": "Reveal Coupon",
+                "type": "button",
+                "url": "http://example.com/coupon/reveal"
+            }
+        ]
+    }
+    return {
+        "user_id": userdb.user_id,
+        "callback_url": "http://localhost:6000/webhook/callback",
+        "text": "Here is our unique promotional coupon! 10% off. Limit 1 per customer.",
+        "attachments": coupon_attachment
+    }
+
+
+def create_media_message(userdb):
+    media_attachment = {
+        "image_url": "https://example.com/goodbye-image.jpg"
+    }
+    return {
+        "user_id": userdb.user_id,
+        "callback_url": "http://localhost:6000/webhook/callback",
+        "text": "No worries! Have a nice day!",
+        "attachments": media_attachment
+    }
